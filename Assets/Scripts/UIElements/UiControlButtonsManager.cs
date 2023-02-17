d00b8cac37c927ead7b3e471e6be9257dba06c63
Bednyakov <bednyakov99@gmail.com>
2023-02-17 19:00:21 +0300

    Добавил анимацию перемешивания кнопок

--- Assets/Scripts/UIElements/UiControlButtonsManager.cs
+++ Assets/Scripts/UIElements/UiControlButtonsManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Descriptors;
 using Player;
@@ -13,16 +14,25 @@ namespace UIElements
 
 		private List<UiControlButton> _uiControlButtons;
 		private PlayerMovement _playerMovement;
+		private UiControlsButtonAnimator _uiControlsButtonAnimator;
 
 		private void Awake()
 		{
 			_uiControlButtons = FindObjectsOfType<UiControlButton>().ToList();
 			_playerMovement = FindObjectOfType<PlayerMovement>();
+			_uiControlsButtonAnimator = GetComponent<UiControlsButtonAnimator>();
 			_playerMovement.OnControlsChanged += PlaceButtons;
 		}
 
 		private void PlaceButtons(Dictionary<MovementDirection, ControlButton> evt)
 		{
+			StartCoroutine(TestRoutine(evt));
+		}
+
+		private IEnumerator TestRoutine(Dictionary<MovementDirection, ControlButton> evt)
+		{
+			_uiControlsButtonAnimator.PlayMix();
+			yield return new WaitForSeconds(1f);
 			foreach (UiControlButton uiControlButton in _uiControlButtons) {
 				ControlButton controlButton = evt[uiControlButton.MovementDirection];
 				UiControlButtonDescriptor uiControlButtonDescriptor = _controlButtonDescriptorCollection.RequireDescriptor(controlButton);
