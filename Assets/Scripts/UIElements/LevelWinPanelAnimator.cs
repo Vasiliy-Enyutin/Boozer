5c239d7f50573c75febd4f1db7223cf9d52ccdd4
Bednyakov <bednyakov99@gmail.com>
2023-02-19 13:28:21 +0300

    Добавил экраны победы и поражения

---
+++ Assets/Scripts/UIElements/LevelWinPanelAnimator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UIElements
+{
+	public class LevelWinPanelAnimator : MonoBehaviour
+	{
+		private readonly int LevelWinPanelShowHash = Animator.StringToHash("LevelWinPanelShow");
+		
+		public void PlayShow()
+		{
+			Animator animator = GetComponent<Animator>();
+			animator.Play(LevelWinPanelShowHash);
+		}
+	}
+}
